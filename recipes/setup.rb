6045488232b5141069b2719c4a68bd44f6df35bf
Chris Kimball <drooLz@gmail.com>
2018-07-23 00:43:24 +0000

    reconfigured motd file with a template resource

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -13,11 +13,20 @@ end
 package 'nano'
 package 'vim'
 
-file '/etc/motd' do
-  content 'This server is the property of Chris'
-  action :create
-  owner 'root'
-  group 'root'
+#file '/etc/motd' do
+# content "This server is the property of Chris
+#  HOSTNAME: #{node['hostname']}
+#  IPADDRESS: #{node['ipaddress']}
+#  CPU: #{node['cpu']['0']['mhz']}
+#  MEMORY: #{node['memory']['total']}" 
+#  action :create
+#  owner 'root'
+#  group 'root'
+#end
+
+template '/etc/motd' do
+  source 'motd.erb'
+  action:create
 end
 
 service 'ntpd' do
